9036c3b81fe7a9cb6bbd14b7f13864f388db9068
matthew <mattclarkeruns@gmail.com>
2017-04-25 01:20:44 -0400

    add makeHoney button and spawnBee button
    This commit will create the addHoney and spawnBee buttons.
    These buttons will update exchange resources.
    Bee cost will increase exponentially.

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
 var pollen = 0;
-var worker_bees = 0;
+var honey = 0;
+var workerBees = 0;
+var workerBeeCost = 2;
+
+function updateResourceAmount() {
+	document.getElementById("pollen").innerHTML = pollen;
+	document.getElementById("honey").innerHTML = honey;
+	document.getElementById("workerBees").innerHTML = workerBees;
+	document.getElementById("workerBeeCost").innerHTML = workerBeeCost;
+}
+
 
 function gatherPollen(amount) {
 	pollen += amount;
-	document.getElementById("pollen").innerHTML = pollen;
+	updateResourceAmount();
 };
+
+function makeHoney(amount) {
+	if (pollen>=5) {
+		pollen-=5;
+		honey+=1;
+		updateResourceAmount();
+	}
+}
+
+function spawnBee(amount) {
+	if(honey >= workerBeeCost) {
+		honey-=2;
+		workerBees+=1;
+		workerBeeCost = Math.floor(1.2*(workerBeeCost+1))
+		updateResourceAmount();
+	}
+}
