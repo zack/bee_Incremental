f3d1c408f41637a6dc1fc5e774f59871a78bfe7d
matthew <mattclarkeruns@gmail.com>
2017-04-30 21:44:37 -0400

    rewrite game data architecture
    This commit will rewrite resources, gatherers, and costs, as objects.
    This commit will store all data that would be saved in the "game" object.
    This commit will create updateHTML function.
    This commit will comfirm functions to new data architecture.

--- main.js
+++ main.js
@@ -7,76 +7,86 @@ Notes:
 	However, this approach may make the code less easy to read.
 */
 
-// Resources
-var pollen = 100;
-var honey = 100;
-var workerBees = 0;
-var workerBeeCost = 2;
-var scienceHoney = 100;
-var time = 0;
+//Everything that is saved is stored here
+var game = {
+	resources: {
+		"pollen": 			100,
+		"honey":				100,
+		"scienceHoney":	100,
+	},
+	gatherers: {
+		"workerBees":		1,
+	},
+	costs: {
+		"workerBeesCost": 2,
+	},
+	time: 0,
+}
 
-// upgrades
-var upgrades = {
-	"workerBeesHoney": {
-		"multiplier": 1,
-		"improvedFlightUnlocked": false,
-		"improvedFlightValue": 3,
-	}
-};
-
-// Time tracker
+// Game state updater
 window.setInterval(function() {
-	time +=.5;
-	day = Math.floor(time % 200)+1
-	year = Math.floor(time / 200)
+	//time
+	game.time +=.5;
+	day = Math.floor(game.time % 200)+1
+	year = Math.floor(game.time / 200)
 	document.getElementById("year").innerHTML = year;
 	document.getElementById("day").innerHTML = day;
 
-	gatherPollen(workerBees*upgrades.workerBeesHoney.multiplier);
+	//gatherer update
+	gatherPollen(game.gatherers.workerBees);
+	updateHTML();
 
 }, 1000);
 
-function updateResourceAmount() {
-	document.getElementById("pollen").innerHTML = pollen;
-	document.getElementById("honey").innerHTML = honey;
-	document.getElementById("workerBees").innerHTML = workerBees;
-	document.getElementById("workerBeeCost").innerHTML = workerBeeCost;
-	document.getElementById("scienceHoney").innerHTML = scienceHoney;
+function updateHTML() {
+	for (var resource in game.resources) {
+    if (!game.resources.hasOwnProperty(resource)) {
+        continue;
+    }
+    document.getElementById(resource).innerHTML = game.resources[resource];
+	}
+	for (var gatherer in game.gatherers) {
+		if(!game.gatherers.hasOwnProperty(gatherer)) {
+			continue;
+		}
+		document.getElementById(gatherer).innerHTML = game.gatherers[gatherer];
+	}
+	for (var cost in game.costs) {
+		if(!game.costs.hasOwnProperty(cost)) {
+			continue;
+		}
+		document.getElementById(cost).innerHTML = game.costs[cost];
+	}
 }
 
 
 // Button Functions
 function gatherPollen(amount) {
-	pollen += amount;
-	updateResourceAmount();
+	game.resources.pollen += amount;
+	updateHTML();
 };
 
 function makeHoney(amount) {
-	if (pollen >= 5) {
-		pollen -= 5;
-		honey += 1;
-		updateResourceAmount();
+	if (game.resources.pollen >= 5) {
+		game.resources.pollen -= 5;
+		game.resources.honey += 1;
+		updateHTML();
 	}
 }
 
 function spawnBee(amount) {
-	if(honey >= workerBeeCost) {
-		honey -= workerBeeCost;
-		workerBees += 1;
-		workerBeeCost = Math.floor(1.2*(workerBeeCost+1))
-		updateResourceAmount();
+	if(game.resources.honey >= game.costs.workerBeesCost) {
+		game.resources.honey -= game.costs.workerBeesCost;
+		game.gatherers.workerBees += 1;
+		game.costs.workerBeesCost = Math.floor(1.2*(game.costs.workerBeesCost+1))
+		updateHTML();
 	}
 }
 
 function makeScienceHoney(amount) {
-	if(honey >= amount * 10) {
-		honey -= amount * 10;
-		scienceHoney += amount;
-		updateResourceAmount();
+	if(game.resources.honey >= amount * 10) {
+		game.resources.honey -= amount * 10;
+		game.resources.scienceHoney += amount;
+		updateHTML();
 	}
-}
-
-
-function unlockImprovedFlight() {
-
 }
